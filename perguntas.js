fb95dc5cb57d230338ad6d2367bf72f4d86256c2
luizgustavo0210 <eletrica2019anderson@gmail.com>
2024-11-27 11:09:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,156 @@
+
+criaCartao(
+    
+    'oque é uma estrofe?',
+    'é cada um dos “bloquinhos” que compõem o poema.'
+)
+criaCartao(
+    
+    'O que é o Bitcoin?',
+    'Bitcoin é uma criptomoeda, uma moeda digital descentralizada'
+)
+criaCartao(
+    'Quem criou o Bitcoin?',
+    'O Bitcoin foi criado por uma pessoa ou grupo sob o pseudônimo de Satoshi Nakamoto',
+)
+    criaCartao(
+    
+    '3. Como o Bitcoin é armazenado?',
+    'O Bitcoin é armazenado em carteiras digitais (wallets).'
+)
+criaCartao(
+    
+    'O que é mineração de Bitcoin?',
+    'Mineração é o processo de validar transações e criar novos bitcoins, usando poder computacional.'
+)
+criaCartao(
+    
+    'O Bitcoin é anônimo?',
+    'O Bitcoin oferece certo anonimato, mas todas as transações são registradas em um livro público chamado blockchain.'
+)
+criaCartao(
+    
+    'O que é PIB?',
+    'PIB é o Produto Interno Bruto, que mede a produção de bens e serviços de um país.'
+)
+criaCartao(
+    
+    'O que significa inflação?',
+    'Inflação é o aumento geral dos preços de bens e serviços ao longo do tempo.'
+)
+criaCartao(
+   
+    'O que é uma taxa de juros?',
+    'Taxa de juros é o valor cobrado pelo empréstimo de dinheiro ou o rendimento sobre investimentos.'
+)
+criaCartao(
+    
+    'O que é uma crise econômica?',
+    'Crise econômica é uma fase de declínio significativo na economia, com aumento de desemprego e redução de produção.'
+)
+criaCartao(
+    
+    'Qual a função do Banco Central?',
+    'O Banco Central regula a moeda e a política monetária, controlando a inflação e a estabilidade econômica.'
+)
+criaCartao(
+    
+    'Qual é o menor instrumento de cordas?',
+    'O violino é o menor.'
+)
+criaCartao(
+    
+    'Como é produzido o som no violão?',
+    'O som é produzido ao dedilhar as cordas.'
+)
+criaCartao(
+    
+    'Qual instrumento de cordas usa um arco?',
+    'O violino, a viola e o violoncelo usam arco'
+)
+criaCartao(
+    
+    'Em qual estilo musical o contrabaixo é comum?',
+    'O contrabaixo é comum no jazz e na música clássica'
+    
+)
+criaCartao(
+   
+    'O que diferencia o bandolim do violino?',
+    'O bandolim é dedilhado, enquanto o violino é tocado com arco.'
+)
+criaCartao(
+    
+    'Quantos times participam da Série A do Brasileirão?',
+    'A Série A do Brasileirão conta com 20 clubes, que disputam o campeonato no sistema de pontos corridos em 38 rodadas.'
+)
+criaCartao(
+    
+    'Qual clube tem mais títulos na história do Brasileirão?',
+    'O Palmeiras é o clube com mais títulos na história do Brasileirão, com 11 conquistas'
+)
+criaCartao(
+    
+    'Quem é o maior artilheiro de uma única edição do Brasileirão?',
+    'O recorde de maior artilheiro em uma única edição é de Washington, com 34 gols, pelo Atlético Paranaense em 2004.'
+)
+criaCartao(
+   
+    'Quem foi o jogador mais jovem a atuar em uma partida do Brasileirão?',
+    'O recorde pertence a Coutinho, que estreou pelo Santos com apenas 14 anos em 1958'
+)
+criaCartao(
+    
+    'Qual foi a maior goleada registrada no Brasileirão?',
+    'A maior goleada foi o placar de 10x0 aplicado pelo Corinthians sobre o Tiradentes-PI em 1983.'
+)
+criaCartao(
+    
+    'Quando foi fundada a Premier League?',
+    'A Premier League foi fundada em 1992.'
+)
+criaCartao(
+    
+    'Quem foi o primeiro campeão da Premier League?',
+    'O Manchester United venceu a primeira edição da Premier League, em 1992-1993.'
+)
+criaCartao(
+    
+    'Quem é o maior artilheiro da história da Premier League?',
+    'Alan Shearer é o maior artilheiro da Premier League, com 260 gols.'
+)
+criaCartao(
+    
+    'Quem foi o jogador mais jovem a estrear na Premier League?',
+    'Harvey Elliott estreou com 16 anos e 30 dias pelo Fulham em 2019.'
+)
+criaCartao(
+    
+    'Qual foi a transferência mais cara da história da Premier League?',
+    'Declan Rice, transferido para o Arsenal por cerca de 105 milhões de libras em 2023, é uma das mais caras.'
+)
+criaCartao(
+    
+    'Qual é o time com mais títulos na história da Premier League?',
+    'O Manchester United é o time com mais títulos na história da Premier League, com 13 conquistas.'
+)
+criaCartao(
+    
+    'Quem foi o jogador com mais assistências na Premier League?',
+    'Ryan Giggs, do Manchester United, é o jogador com mais assistências na Premier League, com 162 assistências.'
+)
+criaCartao(
+    
+    'Qual foi o maior número de gols marcados por um jogador em uma única temporada da Premier League?',
+    'Erling Haaland marcou 36 gols na temporada 2022-2023, estabelecendo o recorde para o maior número de gols em uma única temporada da Premier League.'
+)
+criaCartao(
+    
+    'Quem foi o jogador estrangeiro com mais gols na Premier League?',
+    'Sergio Agüero, da Argentina, é o jogador estrangeiro com mais gols na Premier League, com 184 gols.'
+)
+criaCartao(
+    
+    'Qual foi a maior goleada da história da Premier League?',
+    'A maior goleada da história da Premier League aconteceu quando o Manchester United venceu o Ipswich Town por 9 a 0 em 1995.'
+)
